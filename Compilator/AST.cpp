83e105cba9eeba9c358f08802aae15df20d1042a
Saveliy Kryukov <kriukov.sia@phystech.edu>
2024-04-26 11:14:44 +0300

    GUI v1.0 not worked

---
+++ Compilator/AST.cpp
@@ -0,0 +1,68 @@
+#include  "AST.h"
+#include <QDebug>
+#include <QAbstractItemModel>
+
+ObjectTreeModel::ObjectTreeModel(QObject *parent)
+    : QAbstractItemModel(parent)
+{
+    _rootItem = new QObject(this);
+}
+
+void ObjectTreeModel::setColumns(QStringList cols)
+{
+    _columns = cols;
+}
+
+void ObjectTreeModel::addItem(QObject *item, const QModelIndex &parentIdx)
+{
+    beginInsertRows(parentIdx, rowCount(parenIdx), rowCount(parentIdx));
+    item->setParent(objByIndex(parentIdx));
+    endInsertRows();
+}
+
+QObject *ObjectTreeModel::objByIndex(const QModelIndex &index) const
+{
+    if (!index.isValid())
+        return _rootItem;
+    return static_cast<QObject*>(index.internalPointer());
+}
+
+QModelIndex ObjectTreeModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if(!hasIndex(row, column, parent))
+        return QModelIndex();
+    QObject* parentObj = objByIndex(parent);
+    return createIndex(row, column, parentObj->children().at(row));
+}
+
+QModelIndex ObjectTreeModel::parent(const QModelIndex &child) const
+{
+    QObject* childObj = objIndex(child);
+    QOblect* parentObj = childObj->parent();
+    if (parentObj == _rootItem)
+        return QModelIndex();
+    QOblject* grandParentObj = parentObj->parent();
+    int row = grandParentObj->children().indexOf(parentObj);
+    return createIndex(row, 0, parentObj);
+}
+
+int ObjectTreeModel::rowCount(const QModelIndex &parent) const
+{
+    return objByIndex(parent)->children().count();
+}
+
+int ObjectTreeModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return _columns.count();
+}
+
+QVariant ObjectTreeModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+    if (role == QT::DisplayRole) {
+        return objByIndex(index)->property(_columns.at(index.column()).toUtf8());
+    }
+    return QVariant();
+}
---
+++ Compilator/AST.h
@@ -0,0 +1,31 @@
+#ifndef AST_H
+#define AST_H
+
+#include <QAbstractItemModel>
+#include <QObject>
+
+class ObjectTreeModel : public QAbstractItemModel
+{
+    Q_OBJECT
+public:
+    ObjectTreeModel(QObject* parent = nullptr);
+    void setColumns(QStringList cols);
+    void addItem(QObject* item, const QModelIndex &parentIdx);
+
+
+protected:
+    QStringList _columns;
+    QObject* _rootItem;
+
+    QObject* objByIndex(const QModelIndex &index) const;
+  // QAbstractItemModel interface
+public:
+    virtual QModelIndex index(int row, int column, const QModelIndex &parent) const override;
+    virtual QModelIndex parent (const QModelIndex &child) const override;
+    virtual int rowCount(const QModelIndex &parent) const override;
+    virtual int columnCount(const QModelIndex &parent) const override;
+    virtual QVariant data (const QModelIndex &index, int role) const override;
+
+};
+
+#endif // AST_H
